ea42fd0697d3d35333a8220b30834b539eef2f8b
Nico <nicholas.computacao@outlook.com>
2022-06-07 21:15:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const uniq = require('lodash/uniq');
+
+const items = [
+    'bombolê',
+    'bola',
+    'cachorro',
+    'ave',
+    'cavalo',
+    'gato',
+    'cachorro',
+    'cavalo',
+    'gato'
+];
+
+console.log(process.env.ENVIRONMENT_NAME);
+console.log('-------- OLD ITEMS -------');
+console.log(items);
+console.log('-------- NEW ITEMS -------');
+console.log(uniq(items));
